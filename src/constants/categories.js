6c8b55d2b874b6f7b4071636449585cd11602179
zsamotw <donkemot@interia.pl>
2021-03-08 21:57:14 +0100

    Add new categories

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,7 +1,9 @@
 const meeting = { id: 1, slug: 'meeting' }
 const saw = { id: 2, slug: 'saw' }
 const read = { id: 3, slug: 'read' }
+const outside = { id: 4, slug: 'outside' }
+const inside = { id: 5, slug: 'inside' }
 
-const categories = [meeting, saw, read]
+const categories = [meeting, saw, read, outside, inside]
 
 export default categories
