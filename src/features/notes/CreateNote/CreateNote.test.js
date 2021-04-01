cc2741711f0e1d5cd64db06228fe203e9750033a
zsamotw <donkemot@interia.pl>
2021-04-01 20:15:57 +0200

    Add CreateNote tests

--- src/features/notes/CreateNote/CreateNote.test.js
+++ src/features/notes/CreateNote/CreateNote.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { cleanup } from '@testing-library/react'
+import { cleanup, fireEvent, screen } from '@testing-library/react'
 import { render } from '../../../services/test-service'
 import CreateNote from './index'
 
@@ -7,28 +7,30 @@ describe('Test Create Note component', () => {
   afterEach(cleanup)
 
   it('should render all controls', () => {
-//     const { getByPlaceholderText, getByLabelText, getByTestId } = render(
-//       <CreateNote />
-//     )
-//     const titleInput = getByPlaceholderText(
-//       'createNote.inputs.title.placeholder'
-//     )
-//     const descriptionInput = getByPlaceholderText(
-//       'createNote.inputs.description.placeholder'
-//     )
-//     const categorySelect = getByLabelText('createNote.inputs.category.label')
-//     const fileUploadIcon = getByTestId('file-upload-button')
+    const { getByPlaceholderText, getByLabelText, getByTestId } = render(
+      <CreateNote />
+    )
+    const titleInput = getByPlaceholderText(
+      'notes.createNote.inputs.title.placeholder'
+    )
+    const descriptionInput = getByPlaceholderText(
+      'notes.createNote.inputs.description.placeholder'
+    )
+    const categorySelect = getByLabelText(
+      'notes.createNote.inputs.category.label'
+    )
+    const fileUploadIcon = getByTestId('file-upload-button')
 
-//     expect(titleInput).toBeInTheDocument()
-//     expect(descriptionInput).toBeInTheDocument()
-//     expect(categorySelect).toBeInTheDocument()
-//     expect(fileUploadIcon).toBeInTheDocument()
+    expect(titleInput).toBeInTheDocument()
+    expect(descriptionInput).toBeInTheDocument()
+    expect(categorySelect).toBeInTheDocument()
+    expect(fileUploadIcon).toBeInTheDocument()
   })
 
-  it('should not handle createNote function when form is not filled', () => {
-//     const createNoteMock = jest.fn()
-//     render(<CreateNote createNote={createNoteMock} />)
-
-//     expect(createNoteMock).not.toHaveBeenCalled()
+  it('should not handle createNote function when form is not filled', async () => {
+    const createNoteMock = jest.fn()
+    render(<CreateNote createNote={createNoteMock} />)
+    fireEvent.click(screen.getByText('notes.createNote.buttons.submit'))
+    expect(createNoteMock).not.toHaveBeenCalled()
   })
 })
